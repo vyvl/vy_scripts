762f6c37906f351b15c8dd212c695a528c90b46a
Siddartha Peteti <SiddarthaPeteti@fico.com>
2018-03-24 11:34:44 +0530

    Fixed dictionary script failure when network issue

--- vy_dictionary.lua
+++ vy_dictionary.lua
@@ -3,55 +3,49 @@ http = require("socket.http")
 http.TIMEOUT = 5
 link = "http://www.dictionaryapi.com/api/v1/references/learners/xml/"
 key = "?key=1a78d0a1-8b7e-475e-a56e-6e410310bb07"
-bot ="KILLER-BOT"
+bot =""
 ChatArrival = function(user,data)
 	local s,e,command,arg = string.find( data, "%b<> %p(%w+) (%S+).*|")	
 
 	if command == "dict" then
 		Fetch(user,arg)
-		
-		return true
 	end
 	
 end
 Fetch = function(user,arg)
-	arg = string.lower(arg)
 	url = link..arg..key
 	response,err = http.request(url)
 	if not response then
-		Core.SendToUser(user,"<"..bot.."> \n".."Word not found")
+	Core.SendToUser(user,bot.."\n\t\t\t\t".."Looks like a network issue, please try again later. If persistent PM admin")
 		 return true
 	end
 	local e,s,test = string.find(response,"<dt>.-:(.-)<")
-	
 	if not test then
-		Core.SendToUser(user,"<"..bot.."> \n".."Word not found")
+		Core.SendToUser(user,bot.."\n\t\t\t\t".."Word not found")
 		 return true
 	end
 	test = string.gsub(test,":",",")
 
 	local e,s,example = string.find(response,"<vi>(.-)</vi>")
 	if not example then
-			meaning = test
-			test = arg.." : "..meaning
-		Core.SendToUser(user,"<"..bot.."> \n"..test)
+		Core.SendToUser(user,bot.."\n\t\t\t\t".."Word not found")
 		 return true
 	end
 	example = string.gsub(example,"<it>","")
 	if not example then
-		Core.SendToUser(user,"<"..bot.."> \n".."Word not found")
+		Core.SendToUser(user,bot.."\n\t\t\t\t".."Word not found")
 		 return true
 	end
 	example = string.gsub(example,"</it>","")
 	meaning = test
-	test = arg.." : "..meaning.."\nExample: "..example
+	test = "\n\t\t\t\t\t\t\t\t\tDictionary\n".."\t\t\t\t"..arg.." : "..meaning.."\n\t\t\t\tExample: "..example
 	
 	
 	if not test then
-		 Core.SendToUser(user,"<"..bot.."> \n".."Word not found")
+		 Core.SendToUser(user,bot.."\n\t\t\t\t".."Word not found")
 		 return true
 	end
-	Core.SendToUser(user,"<"..bot.."> \n"..test)
+	Core.SendToUser(user,bot.."\n\t\t\t\t"..test)
 	
 	
 end	
